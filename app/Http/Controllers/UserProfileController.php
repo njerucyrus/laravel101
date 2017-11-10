7b8048452229bfd623c51fd4d9c4e8a9d5615e18
njerucyrus <njerucyrus5@gmail.com>
2017-11-11 00:19:01 +0300

    added logic to userprofile ctrl

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -2,9 +2,50 @@
 
 namespace Blog\Http\Controllers;
 
+use Blog\UserProfile;
 use Illuminate\Http\Request;
+
 
 class UserProfileController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $user = $request->user();
+        return view('create_profile', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+      $profile = new UserProfile;
+      $profile->country = $request->input('country');
+      $profile->gender = $request->input('gender');
+      $profile->user_id = $request->user()->id;
+      $profile->save();
+      $request->session()->flash('profile created successfully');
+      return redirect('/home');
+    }
+
+    /*
+     * for showing the profile page
+     *
+     */
+    public function show($id){
+        $profile = UserProfile::findOrFail($id);
+        return view('user_profile', compact('profile'));
+
+    }
+    /*
+     * for showing update form page
+     */
+    public function edit($id){
+        $profile = UserProfile::findOrFail($id);
+        return view('update_profile', compact('profile'));
+    }
+    public function update(Request $request, $id){
+        $profile = UserProfile::find($id);
+        $profile->country = $request->input('country');
+        $profile->gender = $request->input('gender');
+        $profile->save();
+
+    }
 }
