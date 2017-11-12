eee1971680d8c1c11c5de7dc86054d2177a34272
njerucyrus <njerucyrus5@gmail.com>
2017-11-13 02:08:12 +0300

    added likes to the post table

---
+++ database/migrations/2017_11_12_230227_add_likes_to_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLikesToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->bigInteger('likes')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('likes');
+        });
+    }
+}
